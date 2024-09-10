3ce2a8b6d68efa786230292b08230daa62a02bb6
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-09-10 19:38:25 +0900

    updated

--- src/components/CategoriesSection.js
+++ src/components/CategoriesSection.js
@@ -1,68 +1,68 @@
-import { backdropClasses, Box, Card, CardContent, CardMedia, createTheme, Grid, ThemeProvider, Typography } from '@mui/material'
+import { Box, Card, CardContent, CardMedia, createTheme, Grid, ThemeProvider, Typography } from '@mui/material'
 import React from 'react'
-import techNewsImage from '../assets/1.jpg'
-import gadgetReviewsImage from '../assets/3.jpg'
-import programmingTipsImage from '../assets/4.jpg'
-import softwareUpdateImage from '../assets/5.jpg'
+import techi from '../assets/1.jpg'
+import gadi from '../assets/3.jpg'
+import progi from '../assets/4.jpg'
+import softi from '../assets/5.jpg'
 
 const darkTheme = createTheme({
   palette: {
     mode: 'dark'
-  }, 
+  },
   typography: {
     h3: {
       fontSize: '2rem',
       color: 'white',
-      textAlign: 'center',
+      textAlign:'center',
       marginBottom: '20px'
-    }
+    },
   }
 })
 
 const categories = [
   {
     title: 'Tech News',
-    image: techNewsImage,
+    image: techi,
   },
   {
     title: 'Gadget Reviews',
-    image: gadgetReviewsImage,
+    image: gadi,
   },
   {
     title: 'Programming Tips',
-    image: programmingTipsImage, 
+    image: progi,
   },
   {
     title: 'Software Updates',
-    image: softwareUpdateImage, 
+    image: softi,
   },
-];
+]
 
 const CategoriesSection = () => {
   return (
-    <ThemeProvider theme={darkTheme}> 
-      <Box sx={{backgroundColor:'#121212', padding: '20px'}}>
-        <Typography variant='h3'>Explore Our Categories</Typography>
-        <Grid container spacing={4}>
-          {categories.map((category, index) => (
-            <Grid item xs={12} sm={6} md={3} key={index}>
-              <Card sx={{display:'flex', flexDirection:'column', backgroundColor: '#1e1e1e'}}>
-                <CardMedia
-                  component='img'
-                  image={category.image}
-                  alt={category.title}
-                  sx={{height:140}}
-                >
-                </CardMedia>
-                <CardContent>
-                  <Typography>{category.title}</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    </ThemeProvider>
+   <ThemeProvider theme={darkTheme}>
+    <Box sx={{backgroundColor:'#121212', p:'20px'}}>
+      <Typography variant='h3'>Explore Our Categories</Typography>
+      <Grid container spacing={4}>
+        {categories.map((category, index) => (
+          <Grid item xs={12} sm={6} md={3} key={index}>
+            <Card>
+              <CardMedia
+                component='img'
+                image={category.image}
+                alt={category.title}
+                sx={{height:140}}
+              >
+              </CardMedia>
+              <CardContent>
+                <Typography>{category.title}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+   </ThemeProvider>
   )
 }
 
