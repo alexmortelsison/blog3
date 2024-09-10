1ceadb3443ff31bb5682a10f9e7662ff0ede8d5c
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-09-10 17:06:39 +0900

    update

--- src/components/CategoriesSection.js
+++ src/components/CategoriesSection.js
@@ -1,19 +1,19 @@
-import { Box, Card, CardContent, CardMedia, createTheme, Grid, ThemeProvider, Typography } from '@mui/material'
+import { backdropClasses, Box, Card, CardContent, CardMedia, createTheme, Grid, ThemeProvider, Typography } from '@mui/material'
 import React from 'react'
-import techImg from '../assets/1.jpg'
-import gadgetImg from '../assets/3.jpg'
-import progImg from '../assets/4.jpg'
-import softImg from '../assets/5.jpg'
+import techNewsImage from '../assets/1.jpg'
+import gadgetReviewsImage from '../assets/3.jpg'
+import programmingTipsImage from '../assets/4.jpg'
+import softwareUpdateImage from '../assets/5.jpg'
 
 const darkTheme = createTheme({
   palette: {
     mode: 'dark'
-  },
+  }, 
   typography: {
     h3: {
       fontSize: '2rem',
       color: 'white',
-      textAlign:'center',
+      textAlign: 'center',
       marginBottom: '20px'
     }
   }
@@ -22,37 +22,38 @@ const darkTheme = createTheme({
 const categories = [
   {
     title: 'Tech News',
-    image: techImg,
+    image: techNewsImage,
   },
   {
     title: 'Gadget Reviews',
-    image: gadgetImg,
+    image: gadgetReviewsImage,
   },
   {
     title: 'Programming Tips',
-    image: progImg,
+    image: programmingTipsImage, 
   },
   {
     title: 'Software Updates',
-    image: softImg,
+    image: softwareUpdateImage, 
   },
-]
+];
 
 const CategoriesSection = () => {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Box sx={{padding:'20px', backgroundColor:'#121212'}}>
+    <ThemeProvider theme={darkTheme}> 
+      <Box sx={{backgroundColor:'#121212', padding: '20px'}}>
         <Typography variant='h3'>Explore Our Categories</Typography>
         <Grid container spacing={4}>
           {categories.map((category, index) => (
             <Grid item xs={12} sm={6} md={3} key={index}>
-              <Card sx={{backgroundColor:'#1e1e1e'}}>
-                <CardMedia 
+              <Card sx={{display:'flex', flexDirection:'column', backgroundColor: '#1e1e1e'}}>
+                <CardMedia
                   component='img'
                   image={category.image}
                   alt={category.title}
                   sx={{height:140}}
-                />
+                >
+                </CardMedia>
                 <CardContent>
                   <Typography>{category.title}</Typography>
                 </CardContent>
